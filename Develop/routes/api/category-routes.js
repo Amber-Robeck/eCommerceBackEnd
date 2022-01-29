a0d8a8306a1e831f895a14668692ef545f9253ae
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-28 18:21:17 -0600

    added one route to category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,13 +5,50 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  // // be sure to include its associated Products
+  //this query shows id categoryname id product name price stock category id
+  // Select * from category join product on product.category_id = category.id;
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+//   try {
+//     const categoryData = await Category.findByPk(req.params.id, {
+//       include: [{ model: Product }],
+//       attributes: {
+//         include: [
+//           [
+//             sequelize.literal(
+//               '(Select * from category where product.category_id = category.id)'
+//             ),
+//             '',
+//           ],
+//         ],
+//       },
+//     });
+
+//     if (!categoryData) {
+//       res.status(404).json({ message: 'No categories found with that id!' });
+//       return;
+//     }
+
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 
 router.post('/', (req, res) => {
   // create a new category
