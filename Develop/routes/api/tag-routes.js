3ecd91cd6af4c60b2b1a142a88e1418ddbf44e8c
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-01 06:28:36 -0600

    in tag-routes.js added a put route for updating tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -47,8 +47,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagReturn = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagReturn) {
+      res.status(404).json({ message: 'Could not find an associated tag with that id!' })
+    };
+
+    res.status(200).json(tagReturn);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
