9523e30e282a3a7d89bf2601784c6b914fa68bef
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-01 06:23:32 -0600

    in tag-routes.js added post route for creating new tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -20,8 +20,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-// });
-
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
@@ -39,8 +37,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagReturn = await Tag.create(req.body);
+    res.status(200).json(tagReturn);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
